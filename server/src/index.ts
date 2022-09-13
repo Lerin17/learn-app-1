1c8a7cdda1e76c1fcc98a8c2d9c1857c57148d58
Lerin17 <lerinowoade17@gmail.com>
2022-09-13 12:47:28 +0100

    first commit #1

---
+++ server/src/index.ts
@@ -0,0 +1,65 @@
+// import * as express from "express";
+
+// const PORT = process.env.PORT || 4000
+
+// const app = express()
+
+// const server = app.listen(PORT, () => {
+//     console.log(`listrning on ${PORT}`)
+// })
+
+console.log('hello worl')
+
+import  express from "express";
+const app = express()
+
+const server = require('http').createServer(app)
+const cors = require('cors')
+// import express from 'express'
+
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', "POST"]
+    }
+})
+
+app.use(cors())
+
+const PORT = process.env.PORT || 3022;
+
+app.get('/',(req, res) => {
+return res.send('server is listening')
+})
+
+server.listen(PORT, ()=> {
+        console.log('App listening at loclh')
+    })
+
+io.on('connection', (socket:any) => {
+    socket.emit('me', socket.id)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callended')
+    });
+
+    socket.on('calluser', ({userToCall, signalData, from, name}:any) => {
+        console.log('supa bounce')
+        io.to(userToCall).emit('calluser', {signal: signalData, from, name})
+    })
+
+    socket.on('answercall', (data:any) => {
+        io.to(data.to).emit('callaccepted', data.answer)
+    })
+})
+
+// app.post('/api/data', (req, res) => {
+// console.log(req.body);
+
+// return res.send(200)
+// })
+
+// app.listen(3022, ()=> {
+//     console.log('App listening at localhost')
+// })
